339c099a066635cea0ed3cb0fa04256f406a5fe4
DinaElmazi <72730729+DinaElmazi@users.noreply.github.com>
2021-11-04 14:57:51 +0100

    slight changes

--- FrontEndMiniprojectDina/src/main/java/frontendminiproject/frontendminiprojectdina/Main.java
+++ FrontEndMiniprojectDina/src/main/java/frontendminiproject/frontendminiprojectdina/Main.java
@@ -8,8 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -26,93 +26,102 @@ public class Main extends Application
 
     //All buttons or other elements are defined here.
     @FXML
-    private Button joinButton, connectButton, submitButton;
+    private Button joinButton, connectButton, submitButton, startButton;
     @FXML
-    private TextField ipField, portField, nameField, playerOneTF, playerTwoName;
+    private TextField ipField, portField, nameField;
+    @FXML
+    private Label joinPlayerOneTF;
+    @FXML
+    private Label hostPlayerOneTF;
+
+
     DataOutputStream output;
     DataInputStream input;
     static Socket clientSocket;
     static Boolean isHost = false;
+    static String clientName;
     
 
     //___________________________ Main Start Method _________________________
 
-    public static void main(String[] args)
-    {
-        launch();
-    }
+    public static void main(String[] args) {launch();}
 
 
     //_______________________ FXML Scene shifts and Button Action methods _________________________
 
     //This is the main start function.
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader homePage = new FXMLLoader(Main.class.getResource("HomePage.fxml"));
-
         Scene homeScene = new Scene(homePage.load(), 1080, 600);
-
         stage.setTitle("Jeoparty");
         stage.setScene(homeScene);
         stage.show();
     }
 
-    //
+    //What happens when different buttons are pressed.
     @FXML
-    private void joinButtonPress(ActionEvent event) throws Exception
-    {
-        Stage stage = (Stage) joinButton.getScene().getWindow();
-        FXMLLoader ipPage = new FXMLLoader(Main.class.getResource("ConnectIPPage.fxml"));
-        Scene ipScene = new Scene(ipPage.load(), 1080, 600);
-        stage.setScene(ipScene);
-        stage.show();
-    }
-
-    //Button action method that happens when the button on ConnectIPPage is pressed.
-    @FXML
-    private void connectButtonPressed(ActionEvent event) throws Exception
-    {
-        String ipText = ipField.getText(); //The IP is gotten from the first text field in ConnectIPPage.
-        int portText = Integer.parseInt(portField.getText()); //The port is gotten from the second text field, and typecast/changed to an integer.
-        joinServer(ipText, portText); //we join the server with ip address, and port (The port is typecast/changed to an integer by using parseInt.
-
-        //___________________________________________________________
-
-        Stage stage = (Stage) connectButton.getScene().getWindow();
-        FXMLLoader nameSubmitPage = new FXMLLoader(Main.class.getResource("InputName.fxml"));
-        Scene nameSubmitScene = new Scene(nameSubmitPage.load(), 1080, 600);
-        stage.setScene(nameSubmitScene);
-        stage.show();
-    }
-
-    //Method for when clicking submit in the naming screen. Also sends the written name to the server.
-    @FXML
-    private void goToLobby(ActionEvent event) throws Exception
+    private void handleButtonPresses(ActionEvent event) throws Exception
     {
 
-        Stage stage = (Stage) submitButton.getScene().getWindow();
-        if (event.getSource() == submitButton && !isHost)
+        //JoinButton action.
+        if(event.getSource() == joinButton)
         {
-            System.out.println("loading host not");
-            FXMLLoader joinPage = new FXMLLoader(Main.class.getResource("JoinPage.fxml"));
-            Scene joinScene = new Scene(joinPage.load(), 1080, 600);
-            stage.setScene(joinScene);
+            Stage stage = (Stage) joinButton.getScene().getWindow();
+            FXMLLoader ipPage = new FXMLLoader(Main.class.getResource("ConnectIPPage.fxml"));
+            Scene ipScene = new Scene(ipPage.load(), 1080, 600);
+            stage.setScene(ipScene);
             stage.show();
-            sendNameInput();
         }
 
-        if (event.getSource() == submitButton && isHost)
+        //ConnectButton action.
+        if(event.getSource() == connectButton)
         {
-            System.out.println("loading host plage");
-            FXMLLoader hostPage = new FXMLLoader(Main.class.getResource("HostPage.fxml"));
-            Scene hostScene = new Scene(hostPage.load(), 1080, 600);
-            stage.setScene(hostScene);
+            Stage stage = (Stage) connectButton.getScene().getWindow();
+            String ipText = ipField.getText(); //The IP is gotten from the first text field in ConnectIPPage.
+            int portText = Integer.parseInt(portField.getText()); //The port is gotten from the second text field, and typecast/changed to an integer.
+            joinServer(ipText, portText); //we join the server with ip address, and port (The port is typecast/changed to an integer by using parseInt.
+            //___________________________________________________________
+            FXMLLoader nameSubmitPage = new FXMLLoader(Main.class.getResource("InputName.fxml"));
+            Scene nameSubmitScene = new Scene(nameSubmitPage.load(), 1080, 600);
+            stage.setScene(nameSubmitScene);
             stage.show();
-            sendNameInput();
+        }
 
+        //SubmitButton actions.
+        if(event.getSource() == submitButton)
+        {
+            Stage stage = (Stage) submitButton.getScene().getWindow();
+            clientName = nameField.getText();
+            System.out.println(clientName);
+            hostPlayerOneTF.setText(clientName);
+
+            if (!isHost)
+            {
+                stage = (Stage) joinPlayerOneTF.getScene().getWindow();
+                System.out.println("loading host not");
+                FXMLLoader joinPage = new FXMLLoader(Main.class.getResource("JoinPage.fxml"));
+                Scene joinScene = new Scene(joinPage.load(), 1080, 600);
+                stage.setScene(joinScene);
+                stage.show();
+                sendNameInput();
+            }
+
+            if (isHost)
+            {
+                System.out.println("loading host page");
+                FXMLLoader hostPage = new FXMLLoader(Main.class.getResource("HostPage.fxml"));
+                Scene hostScene = new Scene(hostPage.load(), 1080, 600);
+                stage.setScene(hostScene);
+                stage.show();
+                sendNameInput();
+                setHostPlayerOneTF(clientName);
+            }
         }
 
     }
+
 
     //____________________________ Other important methods _______________________
 
@@ -125,16 +134,19 @@ public class Main extends Application
         System.out.println(isHost);
         setSocket(socket);
     }
-    public void setSocket(Socket socket)
-    {
-        clientSocket = socket;
-    }
 
+    //A method that sets the Socket to the parsed socket.
+    public void setSocket(Socket socket)
+    {clientSocket = socket;}
+
+    //A method for sending the name input through the data output streams.
     public void sendNameInput() throws IOException
     {
-        System.out.println("Socket:" + clientSocket);
-        System.out.println("Name:" + nameField.getText());
         output = new DataOutputStream(clientSocket.getOutputStream());
         output.writeUTF(nameField.getText());
     }
+
+    //Set one of the labels to the name
+    public void setHostPlayerOneTF(String name)
+    {hostPlayerOneTF.setText(name);}
 }
